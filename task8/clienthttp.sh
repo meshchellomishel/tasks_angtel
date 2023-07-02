c81b22a2e492f80d20f29e0024e234eb45e2502c
meshchellomishel <126414636+meshchellomishel@users.noreply.github.com>
2023-07-02 16:42:15 -0400

    Update task8

--- task8/clienthttp.sh
+++ task8/clienthttp.sh
@@ -4,11 +4,14 @@ curl -i -X PUT localhost:10000/op1 -d '5'
 echo post calculate
 curl -i -X POST localhost:10000/calculate
 
+echo puth 4 in index 3
+curl -i -X PUT localhost:10000/op3 -d '4':
+
 echo put 56 in index 2
 curl -i -X PUT localhost:10000/op2 -d '56'
 
-echo get 5 in index 1
-curl -i -X GET localhost:10000/op1 -d '5'
+echo get value from index 1
+curl -i -X GET localhost:10000/op1
 
 echo get value from index 3
 curl -i -X GET localhost:10000/op3
@@ -17,4 +20,4 @@ echo post calculate
 curl -i -X POST localhost:10000/calculate
 
 echo post calculate dot
-curl -i -X POST localhost:10000/calculate -H Operand:'+'
+curl -i -X POST localhost:10000/calculate -H Operation:' *'
