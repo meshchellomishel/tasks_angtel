0b87e072041c7cf0e23f0d4528f4468522d1a24f
name <thetrenshow157@gmail.com>
2023-07-02 17:17:35 -0400

    tasks completed

--- task4/task4.py
+++ task4/task4.py
@@ -28,7 +28,7 @@ def end_prog():
 
 def run_proc(args, subprocesses, In=None, out=sb.PIPE, text=True):
     print(f'running... {args}')
-    
+
     pr = sb.Popen(args, stdin=In, stdout=out, text=text)
     subprocesses += [pr]
     pr.wait()
@@ -36,7 +36,7 @@ def run_proc(args, subprocesses, In=None, out=sb.PIPE, text=True):
 
     print(f'finished')
     return pr
-    
+
 
 def handler(sig_num, frame):
     write_terminated(data)
@@ -50,16 +50,16 @@ def check_file(args):
         return args[-2] == "<"
     except IndexError:
         return False
-    
+
 
 def analyze_args(data, argv, index, subprocesses, stdin=sb.PIPE, stdout=sb.PIPE):
     pr = 0
     if index != len(argv):
         args = argv[index].split()
-        
+
         if check_file(args):
             subproc = args[0:len(args)-2]
-            
+
             with open(args[-1], "w+") as file:
                 data += [about_cmd(args[0], file.name)]
                 pr = run_proc(subproc, subprocesses, In=stdin, out=file)
@@ -67,7 +67,7 @@ def analyze_args(data, argv, index, subprocesses, stdin=sb.PIPE, stdout=sb.PIPE)
         else:
             data += [about_cmd(args[0])]
             pr = run_proc(args, subprocesses, In=stdin, out=stdout)
-        
+
         data[-1]['code'] = pr.returncode
         analyze_args(data, argv, index + 1, subprocesses, pr.stdout)
 
@@ -80,15 +80,15 @@ signal.signal(signal.SIGUSR1, handler)
 data = []
 subprocesses = []
 
-try:
-    if argv.count('|') != 0:
-        argv = argv.split('|')
+if argv:
+    try:
+        if argv.count('|') != 0:
+            argv = argv.split('|')
 
-        analyze_args(data, argv, 0, subprocesses)
-    else:
-        analyze_args(data, [argv], 0, subprocesses)
-except KeyboardInterrupt:
-    write_terminated(data)
+            analyze_args(data, argv, 0, subprocesses)
+        else:
+            analyze_args(data, [argv], 0, subprocesses)
+    except KeyboardInterrupt:
+        write_terminated(data)
 
-end_prog()
-    
+    end_prog()
